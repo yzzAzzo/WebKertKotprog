9ea242a84045577ac31c934993299168cc4833cd
Baranyai Alexander <h845412@stud.u-szeged.hu>
2021-05-30 18:42:10 +0200

    Model

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -34,7 +34,7 @@ export interface CartPrice{
     description: String
     name:String
     price:Number
-    priceAlteration:
+    priceAlteration:PriceAlteration[]
 }
 
 export interface ContactMedium{
@@ -83,9 +83,230 @@ export interface MediumCharacteristic{
 export interface CartItemRelationShip{
     cartItem:CartItemRef[],
     id:String,
-    relationshipType:String
-    
-    
+    relationshipType:String   
+}
+
+export interface CartPrice{
+    description:String,
+    name:String,
+    price:Number,
+    priceAlteration:PriceAlteration[],
+    priceType:String,
+    productOfferingPrice:ProductOfferingPriceRef,
+    recurringChargePeriod:String,
+    unitOfMeasure:String
+}
+
+export interface CartTerm{
+    name:String,
+    value:any,
+    valueType:String
+}
+
+export interface ContactMedium{
+    characteristic:MediumCharacteristic,
+    mediumType:String,
+    preferred:boolean,
+    validFor:TimePeriod
+}
+
+export interface MediumCharacteristic{
+    city:String,
+    contactType:String,
+    country:String,
+    emailAddress:String,
+    faxNumber:String,
+    phoneNumber:String,
+    postCode:String,
+    socialNetworkId:String,
+    stateOrProvince:String,
+    street1:String,
+    street2:String
+}
+
+export interface Money{
+    unit:String,
+    value:Number
+}
+export interface Note{
+    author:String,
+    date:Date,
+    id:String,
+    text:String
+}
+export interface Price{
+    dutyFreeAmount:Money,
+    percentage:Number,
+    taxIncludedAmount:Money,
+    taxRate:Number
+}
+
+export interface PriceAlteration{
+    applicationDuration:Number,
+    description:String,
+    name:String,
+    price:Price,
+    priceType:String,
+    priority:Number,
+    productOfferingPrice:ProductOfferingPriceRef,
+    recurringChargePeriod:String,
+    unitOfMeasure:String
+}
+export interface ProductPrice{
+    billingAccount:BillingAccountRef,
+    description:String,
+    name:String,
+    price:Price,
+    priceType:String,
+    productOfferingPrice:ProductOfferingPriceRef,
+    productPriceAlteration:PriceAlteration[],
+    recurringChargePeriod:String,
+    unitOfMeasure:String
+}
+
+export interface ProductRefOrValue{
+    href:String,
+    id:String,
+    name:String,
+    description:String,
+    isBundle:boolean,
+    isCustomerVisible:boolean,
+    orderDate:Date,
+    productSerialNumber:String,
+    startDate:Date,
+    terminationDate:Date,
+    agreement:AgreementItemRef[],
+    billingAccount:BillingAccountRef,
+    place:RelatedPlaceRefOrValue[],
+    product:ProductRefOrValue[],
+    productCharacteristic:Characteristic[],
+    productOffering:ProductOfferingRef,
+    productOrderItem:RelatedProductOrderItem[],
+    productPrice:ProductPrice[],
+    productRelationship:ProductRelationship[],
+    productSpecification:ProductSpecificationRef[],
+    productTerm:ProductTerm[],
+    realizingResource:ResourceRef[],
+    realizingService:ServiceRef[],
+    relatedParty:RelatedParty[],
+    status:ProductStatusType
+}
+
+export interface ProductRelationship{
+    product:ProductRefOrValue,
+    relationshipType:String,
+}
+
+export interface ProductTerm{
+    description:String,
+    duration:Quantity,
+    name:String,
+    validFor:Date,
+
+}
+
+export interface Quantity{
+    amount:Number,
+    units:String
+}
+
+export interface RelatedParty{
+    href:String,
+    id:String,
+    name:String,
+    role:String,
+}
+
+export interface RelatedPlaceRefOrValue{
+    href:String,
+    id:String,
+    name:String,
+    role:String
+}
+
+export interface RelatedProductOrderItem{
+    orderItemAction:String,
+    orderItemId:String,
+    productOrderHref:String,
+    productOrderId:String,
+    role:String,
+}
+
+export interface TargetProductSchema{
+
+}
+
+export interface AgreementItemRef{
+    href:String,
+    id:String,
+    name:String,
+    agreementItemId:String
+}
+
+export interface BillingAccountRef{
+    href:String,
+    id:String,
+    name:String
+}
+
+export interface CartItemRef{
+    id:String
+}
+
+export interface ProductOfferingPriceRef{
+    href:String,
+    id:String,
+    name:String
+}
+
+export interface ProductOffering{
+    href:String,
+    id:String,
+    name:String
+}
+
+export interface ProductSpecificationRef{
+    href:String,
+    id:String,
+    name:String,
+    targetProductSchema:TargetProductSchema,
+    version:String
+}
+
+export interface ResourceRef{
+    href:String,
+    id:String,
+    name:String,
+    value:String
+}
+
+export interface ServiceRef{
+    href:String,
+    id:String,
+    name:String
+}
+export interface ProductStatusType{
+    href:String,
+    id:String,
+    name:String
+}
+
+export interface ProductOfferingRef{
+    href:String,
+    id:String,
+    name:String
+}
+
+export interface ProductRefValue{
+    href:String,
+    id:String,
+    name:String
+}
+
+export interface CartItemStatusType{
+    href:String,
+    id:String,
+    name:String
 }
 
 export interface TimePeriod{
